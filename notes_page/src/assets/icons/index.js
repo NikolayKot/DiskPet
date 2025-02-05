488ceb1d6b09e25cf6ea87aa24f4de3821629732
Nikolay <kolya.kotlyarenko@gmail.com>
2025-02-05 13:32:18 +0500

    first commit

---
+++ notes_page/src/assets/icons/index.js
@@ -0,0 +1,9 @@
+import LoginIcon from './login.svg'
+import CloseIcon from './close.svg'
+import AddIcon from './add.svg'
+import PasswordIcon from './password.svg'
+import EmailIcon from './email.svg'
+import OnPasswordIcon from './on-password.svg'
+import OffPasswordIcon from './off-password.svg'
+
+export { LoginIcon, CloseIcon, AddIcon, PasswordIcon, EmailIcon, OnPasswordIcon, OffPasswordIcon }
